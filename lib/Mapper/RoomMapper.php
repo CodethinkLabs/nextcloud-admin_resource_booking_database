57318c3a63606ecd29254bd478be0646bc78e5c9
phillip <phillipsmyth@codethink.co.uk>
2019-07-18 17:50:51 +0100

    database name length reduction

--- lib/Mapper/RoomMapper.php
+++ lib/Mapper/RoomMapper.php
@@ -33,6 +33,6 @@ class RoomMapper extends AbstractMapper {
 	 * @return string
 	 */
 	protected function getTableName(): string {
-		return 'admin_resource_booking_db_rooms';
+		return 'arb_db_rooms';
 	}
 }
